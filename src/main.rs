6b22aaef313faafb4f18a92e4ec627d6fc42c6b0
Mike Oleshchuk <144498628+Tawxyn@users.noreply.github.com>
2025-01-10 22:15:11 -0800

    args setup
    Set up pattern and seek args for collecting info for oxi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("no search patter given");
+    let seek = std::env::args().nth(2).expect("no search type given");
+
 }
