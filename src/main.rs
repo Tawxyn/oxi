a310e15411ffccb900706d313359ffc29633cc8a
Mike Oleshchuk <144498628+Tawxyn@users.noreply.github.com>
2025-01-10 23:01:22 -0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+extern crate clap;
+use clap::{Parser, Arg};
+use open;
+
+fn parse_args() -> clap::ArgMatches {
+
+}
+
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no search patter given");
-    let seek = std::env::args().nth(2).expect("no search type given");
+    
 
 }
