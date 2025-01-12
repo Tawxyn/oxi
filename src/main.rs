0b0db3ed624675729250c9ed915cabd7ddd51efa
Mike Oleshchuk <144498628+Tawxyn@users.noreply.github.com>
2025-01-11 22:43:48 -0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use webbrowser;
 
 
 #[derive(Parser)]
-#[command(author = "Mike Oleshchuk", version = "0.1.0", about = "Oxi a tool to scrape the internet for 
+#[command(author = "Mike Oleshchuk", version = "0.1.0", about = "Oxi a tool to search the internet for 
         related programming and bug articles and websites.", long_about = None)]
 struct Cli {
-
     query: String,
+
+    #[arg(default_value = "general")]
+    language: String,
 
     #[arg(default_value = "focus")]
     command: String,
@@ -16,7 +18,7 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let url = format!("https://www.google.com/search?q={}", args.query);
+    let url = format!("https://www.google.com/search?q={}+site:stackoverflow.com+OR+site:reddit.com", args.query);
     // Get input
     if webbrowser::open(&url).is_ok() {
         println!("Opened: {}", args.query);
